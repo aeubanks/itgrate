951f13da550929a8b5300fc98165ead075701ff6
Arthur Eubanks <ayueubanks@gmail.com>
2020-07-11 22:39:39 -0700

    Read file from command line into string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt()]
+struct Opts {
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+}
+
+fn main() -> std::io::Result<()> {
+    let opts = Opts::from_args();
+    std::fs::read_to_string(opts.input)?;
+    Ok(())
 }
