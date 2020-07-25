08ffcadd1cf11fcf06c08dbeadc5d62efe8302ef
Arthur Eubanks <ayueubanks@gmail.com>
2020-07-25 11:33:19 -0700

    New training update

--- src/rate/state.rs
+++ src/rate/state.rs
@@ -12,11 +12,11 @@ pub struct StepParams {
 impl Default for StepParams {
     fn default() -> Self {
         Self {
-            base_fatigue_per_step: 4.8647046,
-            fatigue_per_step_ratio: 32.294823,
-            fatigue_dist_ratio: 1.6507491,
-            fatigue_decay_rate: 0.020347526,
-            rest_time_add_constant: 0.5731187,
+            base_fatigue_per_step: 26.582926,
+            fatigue_per_step_ratio: 63.997425,
+            fatigue_dist_ratio: 0.65589476,
+            fatigue_decay_rate: 0.046889745,
+            rest_time_add_constant: 2.4311178,
         }
     }
 }
